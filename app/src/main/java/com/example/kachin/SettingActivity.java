1db07bcb0f35801972522d879656a9622806b87d
xiangho0325 <xiangho0325@1utar.my>
2024-08-19 16:12:42 +0800

    profile

--- app/src/main/java/com/example/kachin/SettingActivity.java
+++ app/src/main/java/com/example/kachin/SettingActivity.java
@@ -1,9 +1,17 @@
 package com.example.kachin;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Locale;
+
 public class SettingActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +23,7 @@ public class SettingActivity extends AppCompatActivity {
         tvLanguage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle language change logic here
+                showLanguageDialog();
             }
         });
 
@@ -23,9 +31,69 @@ public class SettingActivity extends AppCompatActivity {
         tvHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle help page logic here
+                showHelpDialog();
             }
         });
     }
-}
 
+    private void showLanguageDialog() {
+        final String[] languages = {"English", "Chinese", "Malay"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Language");
+        builder.setItems(languages, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        setLocale("en");
+                        break;
+                    case 1:
+                        setLocale("zh");
+                        break;
+                    case 2:
+                        setLocale("ms");
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void setLocale(String langCode) {
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = resources.getConfiguration();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+
+        Log.d("Locale", "Setting locale to: " + langCode);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            config.setLocale(locale);
+        } else {
+            config.locale = locale;
+        }
+
+        resources.updateConfiguration(config, dm);
+        getSharedPreferences("Settings", MODE_PRIVATE).edit().putString("My_Lang", langCode).apply();
+        recreate();
+    }
+
+    private void showHelpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Help");
+        builder.setMessage("Coming soon \n\n" +
+                "1. \n" +
+                "2. \n" +
+                "3. ");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+}
