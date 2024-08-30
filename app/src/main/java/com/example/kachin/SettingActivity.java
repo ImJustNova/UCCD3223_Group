7bc386f75d101403ee524470fe352fa3091c7f32
xiangho0325 <xiangho0325@1utar.my>
2024-08-30 23:30:40 +0800

    langauge 4

--- app/src/main/java/com/example/kachin/SettingActivity.java
+++ app/src/main/java/com/example/kachin/SettingActivity.java
@@ -2,6 +2,7 @@ package com.example.kachin;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -38,6 +39,7 @@ public class SettingActivity extends AppCompatActivity {
                 showHelpDialog();
             }
         });
+
         // Update texts after setting up the views
         updateTexts();
     }
@@ -49,17 +51,42 @@ public class SettingActivity extends AppCompatActivity {
         builder.setItems(languages, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                String langCode = "";
                 switch (which) {
                     case 0:
-                        setLocale("en");
+                        langCode = "en";
                         break;
                     case 1:
-                        setLocale("zh");
+                        langCode = "zh";
                         break;
                     case 2:
-                        setLocale("ms");
+                        langCode = "ms";
                         break;
                 }
+                confirmLanguageChange(langCode);
+            }
+        });
+        builder.show();
+    }
+
+    private void confirmLanguageChange(final String langCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change Language");
+        builder.setMessage("Do you want to change the language?");
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setLocale(langCode);
+                Intent intent = new Intent(SettingActivity.this, HomePageActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
             }
         });
         builder.show();
@@ -81,7 +108,7 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void loadLocale() {
-        String langCode = getSharedPreferences("Settings", MODE_PRIVATE).getString("My_Lang", "en"); // Default is English
+        String langCode = getSharedPreferences("Settings", MODE_PRIVATE).getString("My_Lang", "en");
         setLocale(langCode);
     }
 
@@ -99,12 +126,12 @@ public class SettingActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Help");
         builder.setMessage("Welcome to Kachin! Here's how you can use the app. \n\n" +
-                "1.Use the bottom navigation bar to quickly access different sections like Home, Add expense/income, History, Report and Profile. \n\n" +
-                "2.On the Home screen, you can get a quick overview of your financial status, including a summary of recent transactions and an overview of your budget.\n\n" +
-                "3.To add a new expense, tap on the '+' button located at the bottom of the screen. Enter the amount, category, date, and any additional notes, then tap 'Save'.\n\n"+
-                "4.You can view all your past expenses by navigating to the 'History' section. \n\n"+
-                "5.In the 'Report' section, you can generate detailed financial reports, including expense breakdowns by category, monthly summaries, and more. This helps you analyze your spending patterns and make informed financial decisions.\n\n" +
-                "6.In the Profile section, you can manage your settings, logout and edit your profile information.\n\n" +
+                "1. Use the bottom navigation bar to quickly access different sections like Home, Add expense/income, History, Report and Profile. \n\n" +
+                "2. On the Home screen, you can get a quick overview of your financial status, including a summary of recent transactions and an overview of your budget.\n\n" +
+                "3. To add a new expense, tap on the '+' button located at the bottom of the screen. Enter the amount, category, date, and any additional notes, then tap 'Save'.\n\n" +
+                "4. You can view all your past expenses by navigating to the 'History' section. \n\n" +
+                "5. In the 'Report' section, you can generate detailed financial reports, including expense breakdowns by category, monthly summaries, and more. This helps you analyze your spending patterns and make informed financial decisions.\n\n" +
+                "6. In the Profile section, you can manage your settings, logout and edit your profile information.\n\n" +
                 "Thank you for using Kachin! We hope you have a great experience.");
 
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -113,7 +140,6 @@ public class SettingActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
-
         builder.show();
     }
 }
